148204f10073f6f3c8c2f126f1eef4fed210f933
Ведерников Иван Валерьевич <IVedernikov@alfabank.ru>
2023-01-10 23:14:41 +0300

    routing, new componets Form and ProductList

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import "./Form.css";
+
+const Form = () => {
+  const [country, setCountry] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [street, setStreet] = React.useState("");
+  const [subject, setSubject] = React.useState("physical");
+
+  const { tg } = useTelegram();
+
+  React.useEffect(() => {
+    tg.MainButton.setParams({ text: "Отправить данные" });
+  }, [tg]);
+
+  React.useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [country, street, tg]);
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+  const onChangeCity = (e) => {
+    setCity(e.target.value);
+  };
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
+
+  const onChangeSubject = (e) => {
+    setSubject(e.target.value);
+  };
+
+  return (
+    <div className={"form"}>
+      <h3>Введите ваши данные</h3>
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Страна"}
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Город"}
+        value={city}
+        onChange={onChangeCity}
+      />
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Улица"}
+        value={street}
+        onChange={onChangeStreet}
+      />
+
+      <select className={"select"} value={subject} onChange={onChangeSubject}>
+        <option value={"physical"}>Физ. лицо</option>
+        <option value={"legend"}>Юр. лицо</option>
+      </select>
+    </div>
+  );
+};
+
+export default Form;
